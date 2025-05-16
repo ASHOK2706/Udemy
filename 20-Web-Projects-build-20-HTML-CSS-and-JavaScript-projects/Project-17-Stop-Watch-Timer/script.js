762a41afa536f0b0759dd6e78e37fb84b9f397f3
ASHOK2706 <ashokkumar27b@gmail.com>
2025-05-16 12:52:47 +0530

    Stop-Watch-Timer

---
+++ 20-Web-Projects-build-20-HTML-CSS-and-JavaScript-projects/Project-17-Stop-Watch-Timer/script.js
@@ -0,0 +1,92 @@
+const minutesLabel = document.getElementById("minutes");
+const secondslabel = document.getElementById("seconds");
+const millisecondsLabel = document.getElementById("milliseconds");
+
+const startButton = document.getElementById("startBtn");
+const stopButton = document.getElementById("stopBtn");
+const pauseButton = document.getElementById("pauseBtn");
+const resetButton = document.getElementById("resetBtn");
+
+const lapList = document.getElementById("laplist");
+
+// stop Watch variables
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let intervel;
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+pauseButton.addEventListener("click", pauseTimer);
+resetButton.addEventListener("click", resetTimer);
+
+stopButton.disabled = true;
+
+function startTimer() {
+    intervel = setInterval(updateTimer, 10)
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopTimer() {
+    clearInterval(intervel);
+    addToLapList();
+    resetTimerData();
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+function pauseTimer() {
+    clearInterval(intervel);
+    startButton.disabled = false;
+}
+
+function resetTimer() {
+    clearInterval(intervel);
+    resetTimerData();
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    
+}
+
+function updateTimer() {
+    milliseconds++;
+    if (milliseconds === 100) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+    }
+
+    displayTimer();
+}
+
+function displayTimer() {
+    millisecondsLabel.textContent = padTime(milliseconds);
+    secondslabel.textContent = padTime(seconds);
+    minutesLabel.textContent = padTime(minutes);
+}
+
+function padTime(time) {
+    return time.toString().padStart(2, '0');
+}
+
+function resetTimerData() {
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    displayTimer();
+}
+
+function addToLapList() {
+    const lapTime = `${padTime(minutes)} : ${padTime(seconds)} : ${padTime(milliseconds)}`;
+
+    const listItem = document.createElement("li");
+
+    listItem.innerHTML = `<span> Lap ${lapList.childElementCount + 1}</span> ${lapTime}`;
+
+    lapList.appendChild(listItem);
+}
